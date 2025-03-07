31067d5e4924c580758b1f160c5a9396e1d42fbf
chandan kumar <chandankumar700451@gamil.com>
2025-03-08 00:01:25 +0530

    make a public1 folder

--- init.js
+++ init.js
@@ -6,7 +6,7 @@ main()
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 async function main(){
-    await mongoose.connect('mongodb://localhost:27017/WhatApp')
+    await mongoose.connect('mongodb://localhost:27017/WhatApp') 
     };
 
     
